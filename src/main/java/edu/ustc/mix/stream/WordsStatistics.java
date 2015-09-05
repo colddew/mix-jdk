7894e32f1f5707e885c6b49c6857b9649e3fc2b7
colddew <88028842@qq.com>
2015-09-05 19:53:52 +0800

    add stream generation and conversion

--- src/main/java/edu/ustc/mix/stream/WordsStatistics.java
+++ src/main/java/edu/ustc/mix/stream/WordsStatistics.java
@@ -7,7 +7,7 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
-public class CountWords {
+public class WordsStatistics {
 	
 	public static void main(String[] args) throws IOException {
 		
