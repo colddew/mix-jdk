b382bcc0605f9d71e6346380dd0583c78c953fdc
colddew <88028842@qq.com>
2015-09-05 22:39:56 +0800

    add stream status transformation and aggregation

---
+++ src/main/java/edu/ustc/mix/stream/StreamAggregation.java
@@ -0,0 +1,45 @@
+package edu.ustc.mix.stream;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * 聚合操作为终止操作，当一个流使用了终止操作后不能再进行其它操作
+ */
+public class StreamAggregation {
+	
+	public static void main(String[] args) throws IOException {
+		
+		String contents = new String(Files.readAllBytes(Paths.get("/Users/workspace-mars/mix-jdk/src/main/java/edu/ustc/mix/stream", "alice.txt")), 
+			StandardCharsets.UTF_8);
+		List<String> wordList = Arrays.asList(contents.split("[\\P{L}]+"));
+		Stream<String> words = wordList.stream();
+		
+		Optional<String> largest = words.max(String::compareToIgnoreCase);
+		if (largest.isPresent()) {
+			System.out.println("largest: " + largest.get());
+		}
+		
+		words = wordList.stream();
+		Optional<String> firstWordStartsWithQ = words.filter(s -> s.startsWith("Q")).findFirst();
+		System.out.println("firsWordtStartsWithQ: " + firstWordStartsWithQ.get());
+		
+		words = wordList.stream();
+		boolean aWordStartsWithQ = words.parallel().anyMatch(s -> s.startsWith("Q"));
+		System.out.println("aWordStartsWithQ: " + aWordStartsWithQ);
+		
+		words = wordList.stream();
+		Optional<String> startsWithQ = words.parallel().filter(s -> s.startsWith("Q")).findAny();
+		if (startsWithQ.isPresent()) {
+			System.out.println("startsWithQ: " + startsWithQ.get());
+		} else {
+			System.out.println("No word starts with Q");
+		}
+	}
+}
---
+++ src/main/java/edu/ustc/mix/stream/StreamStatusTransformation.java
@@ -0,0 +1,41 @@
+package edu.ustc.mix.stream;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamStatusTransformation {
+	
+	public static void main(String[] args) throws IOException {
+		
+		Stream<String> uniqueWords = Stream.of("merrily", "merrily", "merrily", "gently").distinct();
+		StreamUtils.show("uniqueWords", uniqueWords);
+		
+		String contents = new String(Files.readAllBytes(Paths.get("/Users/workspace-mars/mix-jdk/src/main/java/edu/ustc/mix/stream", 
+			"alice.txt")), StandardCharsets.UTF_8);
+		List<String> wordList = Arrays.asList(contents.split("[\\P{L}]+"));
+		Stream<String> words = wordList.stream();
+		StreamUtils.show("words", words);
+		
+		words = wordList.stream();
+		Stream<String> distinct = words.distinct();
+		StreamUtils.show("distinct", distinct);
+		
+		words = wordList.stream();
+		Stream<String> sorted = words.sorted();
+		StreamUtils.show("sorted", sorted);
+		
+		words = wordList.stream();
+		Stream<String> distinctSorted = words.distinct().sorted();
+		StreamUtils.show("distinctSorted", distinctSorted);
+		
+		words = wordList.stream();
+		Stream<String> longestFirst = words.sorted(Comparator.comparing(String::length).reversed());
+		StreamUtils.show("longestFirst", longestFirst);
+	}
+}
