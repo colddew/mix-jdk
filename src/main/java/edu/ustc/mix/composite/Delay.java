f29b63af45353acecda7b16453fb843fb3738ca7
colddew <88028842@qq.com>
2015-09-18 00:50:02 +0800

    add delay and parallel operation

---
+++ src/main/java/edu/ustc/mix/composite/Delay.java
@@ -0,0 +1,65 @@
+package edu.ustc.mix.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+class LatentImage {
+	
+	private Image in;
+	private List<UnaryOperator<Color>> pendingOperations;
+	
+	public static LatentImage from(Image in) {
+		
+		LatentImage result = new LatentImage();
+		result.in = in;
+		result.pendingOperations = new ArrayList<>();
+		
+		return result;
+	}
+	
+	LatentImage transform(UnaryOperator<Color> f) {
+		pendingOperations.add(f);
+		return this;
+	}
+	
+	public Image toImage() {
+		
+		int width = (int) in.getWidth();
+		int height = (int) in.getHeight();
+		WritableImage out = new WritableImage(width, height);
+		
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				Color c = in.getPixelReader().getColor(x, y);
+				for (UnaryOperator<Color> f : pendingOperations) {
+					c = f.apply(c);
+				}
+				out.getPixelWriter().setColor(x, y, c);
+			}
+		}
+		
+		return out;
+	}
+}
+
+public class Delay extends Application {
+	
+	public void start(Stage stage) {
+		
+		Image image = new Image(CompositeUtils.IMAGE_PATH_EIFFEL);
+		Image finalImage = LatentImage.from(image).transform(Color::brighter).transform(Color::grayscale).toImage();
+		
+		stage.setScene(new Scene(new HBox(new ImageView(image), new ImageView(finalImage))));
+		stage.show();
+	}
+}
---
+++ src/main/java/edu/ustc/mix/composite/Parallel.java
@@ -0,0 +1,77 @@
+package edu.ustc.mix.composite;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.function.UnaryOperator;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Parallel extends Application {
+	
+	public static Color[][] parallelTransform(Color[][] in, UnaryOperator<Color> f) {
+		
+		int n = Runtime.getRuntime().availableProcessors();
+		int height = in.length;
+		int width = in[0].length;
+		Color[][] out = new Color[height][width];
+		
+		try {
+			ExecutorService pool = Executors.newCachedThreadPool();
+			
+			for (int i = 0; i < n; i++) {
+				
+				int fromY = i * height / n;
+				int toY = (i + 1) * height / n;
+				
+				pool.submit(() -> {
+					System.out.printf("%s %d...%d\n", Thread.currentThread(), fromY, toY - 1);
+					for (int x = 0; x < width; x++) {
+						for (int y = fromY; y < toY; y++) {
+							out[y][x] = f.apply(in[y][x]);
+						}
+					}
+				});
+			}
+			
+			pool.shutdown();
+			pool.awaitTermination(1, TimeUnit.HOURS);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+		
+		return out;
+	}
+	
+	public void start(Stage stage) {
+		
+		Image image = new Image(CompositeUtils.IMAGE_PATH_EIFFEL);
+		int width = (int) image.getWidth();
+		int height = (int) image.getHeight();
+		Color[][] pixels = new Color[height][width];
+		
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				pixels[y][x] = image.getPixelReader().getColor(x, y);
+			}
+		}
+		pixels = parallelTransform(pixels, Color::grayscale);
+		
+		WritableImage result = new WritableImage(width, height);
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				result.getPixelWriter().setColor(x, y, pixels[y][x]);
+			}
+		}
+		
+		stage.setScene(new Scene(new HBox(new ImageView(image), new ImageView(result))));
+		stage.show();
+	}
+}
