be3947b4606e5842351aa62529e10d63f03dbda3
colddew <88028842@qq.com>
2015-09-23 00:57:08 +0800

    add new String and Number method

---
+++ src/main/java/edu/ustc/mix/miscellaneous/Strings.java
@@ -0,0 +1,15 @@
+package edu.ustc.mix.miscellaneous;
+
+import java.time.ZoneId;
+
+public class Strings {
+	
+	public static void main(String[] args) {
+		
+		String joined = String.join("/", "usr", "local", "bin"); // "usr/local/bin"
+		System.out.println(joined);
+		
+		String ids = String.join(", ", ZoneId.getAvailableZoneIds());
+		System.out.println(ids);
+	}
+}
