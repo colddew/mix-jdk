bbf49b9007d76f7db63c8dc5161ec92132026b3b
colddew <88028842@qq.com>
2015-09-24 01:42:10 +0800

    add new math, collection, comparator methods

---
+++ src/main/java/edu/ustc/mix/miscellaneous/CollectionMethods.java
@@ -0,0 +1,25 @@
+package edu.ustc.mix.miscellaneous;
+
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.List;
+
+public class CollectionMethods {
+	
+	public static void main(String[] args) {
+		
+		List<String> ids = new ArrayList<>(ZoneId.getAvailableZoneIds());
+		ids.removeIf(s -> !s.startsWith("America"));
+		ids.forEach(System.out::println);
+		
+		System.out.println("---");
+		ids.replaceAll(s -> s.replace("America/", ""));
+		ids.forEach(System.out::println);
+		
+		BitSet bits = new BitSet();
+		ids.forEach(s -> bits.set(s.length()));
+		System.out.println(Arrays.toString(bits.stream().toArray()));
+	}
+}
